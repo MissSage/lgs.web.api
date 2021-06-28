a51d9845d1896717f68c784a50828eeffb0def0f
ligoushi <184078718@qq.com>
2021-06-28 11:38:42 +0800

    'push'

--- bats/lgs.web.api.Publish.Linux.sh
+++
@@ -1,6 +0,0 @@
-git pull;
-rm -rf .PublishFiles;
-dotnet build;
-dotnet publish -o /home/lgs.web.api/lgs.web.api.Api/bin/Debug/netcoreapp3.1;
-cp -r /home/lgs.web.api/lgs.web.api.Api/bin/Debug/netcoreapp3.1 .PublishFiles;
-echo "Successfully!!!! ^ please see the file .PublishFiles";
